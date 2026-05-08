45ca446731fcf54bb8de1332ff9745bdaaa54fe8
Chase Allbright <accounts@chaseallbright.com>
2026-05-07 23:57:43 -0400

    feat: update cast to v0.1.1

--- Formula/cast.rb
+++ Formula/cast.rb
@@ -1,12 +1,12 @@
 class Cast < Formula
   desc "AI-powered shell command generator using local Ollama models"
   homepage "https://github.com/Kallbrig/cast"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Kallbrig/cast/releases/download/v#{version}/cast_#{version}_darwin_arm64.tar.gz"
-      sha256 "e5c0644ba69c44df630e8887247ece8a292898ac5f1c0c5007c2426d669321a7"
+      sha256 "61ef68e82e1d8e97357460a4047e75c030804784c66c3310e0b0eb1f49797d5c"
     end
   end
 
